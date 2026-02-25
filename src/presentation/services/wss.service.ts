0450eb728c89070117e740c8c88a3f812c38fcf0
josalopez86 <josa.lopez@live.com.mx>
2026-02-25 10:50:43 -0800

    Added web socket server

---
+++ src/presentation/services/wss.service.ts
@@ -0,0 +1,49 @@
+import { Server } from "http";
+import {WebSocketServer, WebSocket} from "ws";
+
+interface Options{
+    server: Server,
+    path?: string
+}
+
+export class WssService {
+
+    private static _instance: WssService;
+    private wss: WebSocketServer;
+
+    private constructor(options: Options){
+        const { server, path = "/ws"} = options;
+
+        this.wss = new WebSocketServer({server, path});
+        this.start();
+    }
+
+    static get instance(): WssService{
+        if(!WssService._instance)
+        {
+            throw "WssService is not initialized";
+        }
+        return WssService._instance;
+    }
+
+    static initWss(options: Options){
+        WssService._instance = new WssService(options);
+    }
+
+    public start(){
+        this.wss.on("connection", (ws: WebSocket)=>{
+
+            console.log("Client connected.");
+
+        });
+
+        this.wss.on("close", (ws: WebSocket)=>{
+
+            console.log("Client disconnected.");
+
+        });
+    }
+
+
+
+}
